8b373805a09f3da9ec24234d1923ede4a76c2315
hugues.bretin <hugues.bretin@bonitasoft.com>
2020-06-08 16:27:42 +0200

    feat(mvn): add junit5 support for kotlin projects

--- src/main/resources/archetype-resources/src/test/kotlin/__className__Test.kt
+++ src/main/resources/archetype-resources/src/test/kotlin/__className__Test.kt
@@ -1,70 +1,91 @@
 package ${package}
 
-import java.util.Collections
-
 import io.mockk.every
-import io.mockk.mockk
-
+import io.mockk.impl.annotations.MockK
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.junit5.MockKExtension
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.bonitasoft.engine.api.APIAccessor
 import org.bonitasoft.engine.api.ProcessAPI
 import org.bonitasoft.engine.connector.ConnectorValidationException
 import org.bonitasoft.engine.connector.EngineExecutionContext
-import org.junit.Before
-import org.junit.Test
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 
+@ExtendWith(MockKExtension::class)
 class ${className}Test {
 
-    val filter = ${className}()
-    val apiAccessor = mockk<APIAccessor>()
-    val processApi = mockk<ProcessAPI>()
-    val executionContext = mockk<EngineExecutionContext>()
+    @MockK
+    lateinit var apiAccessor: APIAccessor
 
-    @Before
+    @MockK
+    lateinit var processApi: ProcessAPI
+
+    @RelaxedMockK
+    lateinit var executionContext: EngineExecutionContext
+
+    lateinit var filter: ${className}
+
+    @BeforeEach
     fun setUp() {
-        every { apiAccessor.getProcessAPI() } returns processApi
-        every { executionContext.getProcessDefinitionId() } returns 0
-        filter.setAPIAccessor(apiAccessor);
-        filter.setExecutionContext(executionContext);
-    }
+        filter = ${className}()
+        filter.apiAccessor = apiAccessor
+        filter.executionContext = executionContext
 
-    @Test(expected = ConnectorValidationException::class)
-    fun should_throw_exception_if_mandatory_input_is_missing() {
-        filter.setInputParameters(Collections.emptyMap())
-        filter.validateInputParameters()
-    }
-
-    @Test(expected = ConnectorValidationException::class)
-    fun should_throw_exception_if_mandatory_input_is_not_positive_integer() {
-        val parameters = HashMap<String, Any>();
-        parameters.put(${className}.MAXIMUM_WORKLOAD_INPUT, -1)
-        filter.setInputParameters(parameters)
-        filter.validateInputParameters()
-    }
-
-    @Test(expected = ConnectorValidationException::class)
-    fun should_throw_exception_if_mandatory_input_is_not_an_integer() {
-        val parameters = HashMap<String, Any>();
-        parameters.put(${className}.MAXIMUM_WORKLOAD_INPUT, "1")
-        filter.setInputParameters(parameters)
-        filter.validateInputParameters()
+        every { apiAccessor.processAPI } returns processApi
     }
 
     @Test
-    fun should_return_a_list_of_candidates() {
+    fun `should throw exception if mandatory input is missing`() {
+        assertThatThrownBy { filter.validateInputParameters() }
+                .isExactlyInstanceOf(ConnectorValidationException::class.java)
+    }
+
+    @Test
+    fun `should throw exception if mandatory input is not positive integer`() {
+        // Given
+        val parameters = HashMap<String, Any>()
+        parameters.put(${className}.MAXIMUM_WORKLOAD_INPUT, -1)
+        filter.setInputParameters(parameters)
+
+        // When
+        assertThatThrownBy { filter.validateInputParameters() }
+                // Then
+                .isExactlyInstanceOf(ConnectorValidationException::class.java)
+    }
+
+    @Test
+    fun `should throw exception if mandatory input is not an integer`() {
+        // Given
+        val parameters = HashMap<String, Any>()
+        parameters.put(${className}.MAXIMUM_WORKLOAD_INPUT, "1")
+        filter.setInputParameters(parameters)
+
+        // When
+        assertThatThrownBy { filter.validateInputParameters() }
+                // Then
+                .isExactlyInstanceOf(ConnectorValidationException::class.java)
+    }
+
+    @Test
+    fun `should return a list of candidates`() {
+        // Given
         every { processApi.getUserIdsForActor(any(), "MyActor", 0, Integer.MAX_VALUE) } returns listOf(1L, 2L, 3L)
         every { processApi.getNumberOfAssignedHumanTaskInstances(1L) } returns 2L
         every { processApi.getNumberOfAssignedHumanTaskInstances(2L) } returns 3L
         every { processApi.getNumberOfAssignedHumanTaskInstances(3L) } returns 0L
 
-        val parameters = HashMap<String, Any>();
-        parameters.put(${className}.MAXIMUM_WORKLOAD_INPUT, 3)
+        val parameters = HashMap<String, Any>()
+        parameters[${className}.MAXIMUM_WORKLOAD_INPUT] = 3
         filter.setInputParameters(parameters)
 
+        // When
         val candidates = filter.filter("MyActor")
-        assert(
-            candidates.contains(1L)
-                    && candidates.contains(3L)
-                    && !candidates.contains(2L)
-        ) { "Only users with a workload below the maximum can be candidates." }
+
+        // Then
+        assertThat(candidates).`as`("Only users with a workload below the maximum are returned as candidates")
+                .containsExactly(1L, 3L)
     }
 }
