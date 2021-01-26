06fd9af04714d08899192261320e7f61acd339f5
Romain Bioteau <romain.bioteau@bonitasoft.com>
2021-01-26 17:02:21 +0100

    feat(java11): use java 11 version (#12)
    when bonita.version > 7.13.0, java 11 is used to compile the actor
    filter
    with source and target
    
    Closes STUDIO-3797
    Signed-off-by: Romain Bioteau <romain.bioteau@bonitasoft.com>

---
+++ src/test/resources/projects/testActorfilterJava11Project/reference/src/main/java/com/company/bonitasoft/MyJavaActorfilter.java
@@ -0,0 +1,63 @@
+package com.company.bonitasoft;
+
+import java.util.List;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+
+import org.bonitasoft.engine.api.APIAccessor;
+import org.bonitasoft.engine.api.ProcessAPI;
+import org.bonitasoft.engine.connector.ConnectorValidationException;
+import org.bonitasoft.engine.filter.AbstractUserFilter;
+import org.bonitasoft.engine.filter.UserFilterException;
+import org.bonitasoft.engine.identity.User;
+
+public class MyJavaActorfilter extends AbstractUserFilter {
+
+    private static final Logger LOGGER = Logger.getLogger(MyJavaActorfilter.class.getName());
+
+    static final String MAXIMUM_WORKLOAD_INPUT = "maximumWorkload";
+
+    /**
+     * Perform validation on the inputs defined on the actorfilter definition (src/main/resources/actorfilter-java-test.def)
+     * You should: 
+     * - validate that mandatory inputs are presents
+     * - validate that the content of the inputs is coherent with your use case (e.g: validate that a date is / isn't in the past ...)
+     */
+    @Override
+    public void validateInputParameters() throws ConnectorValidationException {
+        checkPositiveIntegerInput(MAXIMUM_WORKLOAD_INPUT);
+    }
+
+    protected void checkPositiveIntegerInput(String inputName) throws ConnectorValidationException {
+        try {
+            Integer value = (Integer) getInputParameter(inputName);
+            if (value == null || value <= 0) {
+                throw new ConnectorValidationException(String.format("Mandatory parameter '%s' must be a positive integer but is '%s'.", inputName, value));
+            }
+        } catch (ClassCastException e) {
+            throw new ConnectorValidationException(String.format("'%s' parameter must be an Integer", inputName));
+        }
+    }
+
+    /**
+     * @return a list of {@link User} id that are the candidates to execute the task where this filter is defined. 
+     * If the result contains a unique user, the task will automatically be assigned.
+     * @see AbstractUserFilter#shouldAutoAssignTaskIfSingleResult()
+     */
+    @Override
+    public List<Long> filter(String actorName) throws UserFilterException {
+        LOGGER.info(String.format("%s input = %s", MAXIMUM_WORKLOAD_INPUT, getInputParameter(MAXIMUM_WORKLOAD_INPUT)));
+        APIAccessor apiAccessor = getAPIAccessor();
+        ProcessAPI processAPI = apiAccessor.getProcessAPI();
+        List<Long> users = processAPI.getUserIdsForActor(getExecutionContext().getProcessDefinitionId(), actorName, 0, Integer.MAX_VALUE);
+        return users.stream()
+                .filter( userId -> !isOverloaded(userId, apiAccessor))
+                .collect(Collectors.toList());
+    }
+    
+
+    private boolean isOverloaded(Long userId, APIAccessor apiAccessor) {
+        return apiAccessor.getProcessAPI().getNumberOfAssignedHumanTaskInstances(userId) >= (Integer) getInputParameter(MAXIMUM_WORKLOAD_INPUT);
+    }
+}
+
